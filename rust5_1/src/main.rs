148628f5741f8e99e73bd57c6d4daa50b7aa3db1
Arai Kotaro <araikotaro19780811@gmail.com>
2021-10-29 01:55:15 +0900

    The Rust Programming Language 日本語版のサンプルを実行

---
+++ rust5_1/src/main.rs
@@ -0,0 +1,30 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn main() {
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    let mut user2 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    
+    user2.email = String::from("anotheremail@example.com");
+
+    let user3 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        ..user1
+    };
+}
